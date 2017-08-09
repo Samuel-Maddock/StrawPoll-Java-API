6088248cde44e0fe8db24244a72cc77f337c6e71
Samuel Maddock <sammaddock@sky.com>
2017-08-09 22:27:27 +0100

    Added an example class
    In a future commit this will have example code.

---
+++ src/main/java/com/samuelmaddock/strawpollwrapper/Example.java
@@ -0,0 +1,15 @@
+package com.samuelmaddock.strawpollwrapper;
+
+import com.google.gson.Gson;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+/**
+ * Created by Samuel-Maddock on 09/08/2017.
+ */
+public class Example {
+    public static void main(String[] args) throws IOException {
+
+    }
+}
