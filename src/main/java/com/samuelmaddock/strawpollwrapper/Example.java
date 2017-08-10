14cd0a25bbb87e75e3390dbdae813c383a55a37b
Samuel Maddock <sammaddock@sky.com>
2017-08-10 19:55:16 +0100

    Added a small example to the example class

--- src/main/java/com/samuelmaddock/strawpollwrapper/Example.java
+++ src/main/java/com/samuelmaddock/strawpollwrapper/Example.java
@@ -1,15 +1,14 @@
 package com.samuelmaddock.strawpollwrapper;
 
-import com.google.gson.Gson;
-
-import java.io.IOException;
-import java.util.ArrayList;
-
 /**
  * Created by Samuel-Maddock on 09/08/2017.
  */
 public class Example {
-    public static void main(String[] args) throws IOException {
-
+    public static void main(String[] args) {
+        StrawPoll strawPoll = new StrawPoll("Test title", "Option 1", "Option 2");
+        strawPoll.setDupCheck(DupCheckType.DISABLED);
+        strawPoll.create();
+        System.out.println(strawPoll.toRawJSON());  //Raw JSON of the created poll
+        System.out.println(strawPoll.getPollURL()); //URL of created poll
     }
 }
