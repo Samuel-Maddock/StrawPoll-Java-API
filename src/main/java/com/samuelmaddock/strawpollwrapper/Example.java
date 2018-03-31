9ab670036add695a3c8886bf8140d7d60a37cc06
Samuel Maddock <sammaddock@sky.com>
2018-04-01 00:07:33 +0100

    Added a (very artificial!) example on using the update() method

--- src/main/java/com/samuelmaddock/strawpollwrapper/Example.java
+++ src/main/java/com/samuelmaddock/strawpollwrapper/Example.java
@@ -17,6 +17,17 @@ public class Example {
         System.out.println(myPoll.toRawJSON());
 
         myPoll = myPoll.retrieve("https://www.strawpoll.me/1"); //Same as using retrieve(1)
-        System.out.println(myPoll.toRawJSON());
+        System.out.println(myPoll); // toString() calls toRawJSON()
+
+        System.out.println(" ");
+        System.out.println("You have 50 seconds to vote on the poll via the below URl to see the update() method work!");
+        System.out.println(strawPoll.getPollURL()); //URL of created poll
+        try {
+            Thread.sleep(50000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        strawPoll.update();
+        System.out.println(strawPoll);
     }
 }
